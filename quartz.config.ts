d4fb16f94a03be3cf23479bb540ca14524d8f275
Lucas Oliveira <lucasomsousa@gmail.com>
2024-10-27 20:28:26 -0300

    Quartz sync: Oct 27, 2024, 8:28 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Lucas' Garden of Explorations",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
